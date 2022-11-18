c2b8aa0c2d80a945f4497f7ea72411ed952bffc0
Krzysztof Mazur <m.krzysztof@gmail.com>
2022-11-18 17:40:05 +0100

    Initial commit

---
+++ clipreverser_csharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clipreverser_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
