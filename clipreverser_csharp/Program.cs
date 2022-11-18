7c29f21a33a6fcd5214421bb328abc97875e9c51
Krzysztof Mazur <m.krzysztof@gmail.com>
2022-11-18 18:08:13 +0100

    Added reversing string in clipboard.

--- clipreverser_csharp/Program.cs
+++ clipreverser_csharp/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace clipreverser_csharp
 {
     internal class Program
     {
+        // reverse a string
+        static string strReverse(string s)
+        {
+            char[] array = s.ToCharArray();
+            Array.Reverse(array);
+
+            return new string(array);
+        }
+
+        [STAThreadAttribute]
         static void Main(string[] args)
         {
+            IDataObject iData = Clipboard.GetDataObject();
+
+            if(iData == null || !iData.GetDataPresent(DataFormats.Text))
+            {
+                Console.WriteLine("No text data in clipboard");
+                return;
+            }
+
+            string text = (String)iData.GetData(DataFormats.Text);
+            string reversedText = strReverse(text);
+
+            Clipboard.SetText(reversedText);
+
+            Console.WriteLine("Text copied to clipboard");
         }
     }
 }
